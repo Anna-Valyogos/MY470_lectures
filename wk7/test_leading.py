cd21a002df7d6fc576303993089749c476b397c8
Anna Valyogos <a.e.valyogos@lse.ac.uk>
2020-11-10 13:42:46 +0100

    commit

---
+++ wk7/test_leading.py
@@ -0,0 +1,14 @@
+# Test examples for leading_substrings function
+
+import unittest
+import tools
+
+class Test_Leading_Substrings(unittest.TestCase):
+    '''Tests for leading_substrings function.'''
+    inputed = ''
+    leading.leading_substrings(inputted)
+    output_expected = ['']
+    self.assertEqual(output_expected, inputted, 'The string is empty')
+
+if __name__ == '__main__':
+    unittest.main()
